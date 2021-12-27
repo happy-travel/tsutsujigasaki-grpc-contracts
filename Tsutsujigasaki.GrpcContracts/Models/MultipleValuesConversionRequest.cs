3d9a28579ab1afa7cc00ae9c89770a4957b1dcfe
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-12-27 10:39:51 +0500

    added contracts

---
+++ Tsutsujigasaki.GrpcContracts/Models/MultipleValuesConversionRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using HappyTravel.Money.Enums;
+
+namespace Tsutsujigasaki.GrpcContracts.Models;
+
+
+[DataContract]
+public class MultipleValuesConversionRequest
+{
+    [DataMember(Order = 1)]
+    public Currencies SourceCurrency { get; set; }
+    
+    [DataMember(Order = 2)]
+    public Currencies TargetCurrency { get; set; }
+
+    [DataMember(Order = 3)] 
+    public List<decimal> Values { get; set; } = new();
+}
---
+++ Tsutsujigasaki.GrpcContracts/Services/IConversionGrpcService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Tsutsujigasaki.GrpcContracts.Models;
+
+namespace Tsutsujigasaki.GrpcContracts.Services;
+
+[ServiceContract(Name = "Conversion")]
+public interface IConversionGrpcService
+{
+    [OperationContract(Name = "Single")]
+    Task<SingleValueConversionResponse> ConvertSingle(SingleValueConversionRequest request);
+    
+    [OperationContract(Name = "Multiple")]
+    Task<MultipleValuesConversionResponse> ConvertMultiple(MultipleValuesConversionRequest request);
+}
